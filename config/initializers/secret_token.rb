eb10db2507c332de7d4e04b852ab03db2f187d63
Ian Mac <Ian@Ian-Macs-iMac.local>
2012-05-01 23:04:12 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PingApp::Application.config.secret_token = '35a2f46dc40aef44843357496ae996dc81d18833368054cf8ca6fbdc27e3652503c893aa6e40fde77e8ab99365d065c19a8f0abbe16593f5ea01ce7d6ca19de1'
