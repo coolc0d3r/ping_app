31632bdccb8b93c80d9f417816e54dbde9076af7
Ian Mac <coolc0d3r@gmail.com>
2012-06-03 09:40:26 -0400

    Added a title helper

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,6 +1,3 @@
 module StaticPagesHelper
 
-    def title(page_title)
-        @content_for_title = page_title.to_s
-    end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+
+  let(:base_title) { "Ian's Ping App" }
+  
   describe "Homepage" do
     it "should have the h1 'Home'" do
       visit '/static_pages/home'
@@ -10,9 +13,8 @@ describe "Static Pages" do
         it "should have the right title" do
         visit '/static_pages/home'
         page.should have_selector('title',
-            text: "My Ping App | Home")
+            text: "#{base_title} | Home")
         end
-    
   end
   
     describe "Help page" do
@@ -24,9 +26,8 @@ describe "Static Pages" do
         it "should have the right title" do
         visit '/static_pages/help'
         page.should have_selector('title',
-            text: "My Ping App | Help")
-        end
-    
+            text: "#{base_title} | Help")
+        end   
   end
   
   describe "About page" do
@@ -38,12 +39,21 @@ describe "Static Pages" do
         it "should have the right title" do
         visit '/static_pages/about'
         page.should have_selector('title',
-            text: "My Ping App | About")
+            text: "#{base_title} | About")
         end
-  
-    
-    
     end
     
+      describe "Contact page" do
+    it "should have the h1 'Contact Us'" do
+        visit '/static_pages/contact'
+        page.should have_selector('h1', text: 'Contact Us')
+        end
+    
+        it "should have the right title" do
+        visit '/static_pages/contact'
+        page.should have_selector('title',
+            text: "#{base_title} | Contact")
+        end
+    end
 
 end
