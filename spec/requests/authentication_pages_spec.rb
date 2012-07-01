a72ca2246d52c2659365ee69370e99985837181e
Cool C0d3r <coolc0d3r@gmail.com>
2012-07-01 18:16:30 -0400

    Added user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -12,7 +12,6 @@ describe "Authentication" do
     end
     
     describe "signin" do
-    
         before { visit signin_path }
         
         describe "with invalid information" do
@@ -20,35 +19,34 @@ describe "Authentication" do
             
           it { should have_selector('title', text: 'Sign in') }
           it { should have_selector('div.alert.alert-error', text: 'Invalid') }
-          end
           
-        describe "after visiting another page" do
-          before { click_link "Home" }
-          it { should_not have_selector('div.alert.alert-error') }
+          describe "after visiting another page" do
+            before { click_link "Home" }
+            it { should_not have_selector('div.alert.alert-error') }
+          end
         end
             
-        describe "with valid information" do
-            let(:user) { FactoryGirl.create(:user) }
-            before { valid_signin(user) }
+      describe "with valid information" do
+        let(:user) { FactoryGirl.create(:user) }
+        before { valid_signin(user) }
         
-            it { should have_selector('title', text: user.name) }
+        it { should have_selector('title', text: user.name) }
             
-            it { should have_link('Users', href: users_path) }
-            it { should have_link('Profile', href: user_path(user)) }
-            it { should have_link('Settings', href: edit_user_path(user)) }
-            it { should have_link('Sign out', href: signout_path) }
+        it { should have_link('Users', href: users_path) }
+        it { should have_link('Profile', href: user_path(user)) }
+        it { should have_link('Settings', href: edit_user_path(user)) }
+        it { should have_link('Sign out', href: signout_path) }
             
-            it { should_not have_link('Sign in', href: signin_path) }
+        it { should_not have_link('Sign in', href: signin_path) }
         
         describe "followed by signout" do
           before { click_link "Sign out" }
-          it { should have_link('Sign in') }
+            it { should have_link('Sign in') }
           
-          it { should_not have_link('Profile') }
-          it { should_not have_link('Settings') }
-          
+            it { should_not have_link('Profile') }
+            it { should_not have_link('Settings') }
         end
-        end
+      end
     end
     
     describe "authorization" do
@@ -62,12 +60,11 @@ describe "Authentication" do
             valid_signin(user)
           end
           
-          describe "after signing in" do
-            
-            it "should render the desired protected page" do
-              page.should have_selector('title', text: 'Edit user')
+            describe "after signing in" do
+                it "should render the desired protected page" do
+                  page.should have_selector('title', text: 'Edit user')
+                end
             end
-          end
         end
         
         describe "in the Users controller" do
@@ -76,46 +73,71 @@ describe "Authentication" do
             before { visit edit_user_path(user) }
             it { should have_selector('title', text: 'Sign in') }
           end
-        
+          
           describe "submitting to the update action" do
             before { put user_path(user) }
             specify { response.should redirect_to(signin_path) }
-            end
           end
           
           describe "visitng the user index" do
             before { visit users_path }
             it { should have_selector('title', text: 'Sign in') }
-            end
           end
+          
+          describe "visiting the following page" do
+            before { visit following_user_path(user) }
+            it { should have_selector('title', text: 'Sign in') }
+          end
+          
+          describe "visiting the followers page" do
+            before { visit followers_user_path(user) }
+            it { should have_selector('title', text: 'Sign in') }
+          end
+        
+        end
+          
+        describe "in the Microposts controller" do
       
-      describe "in the Microposts controller" do
-      
-        describe "submitting to the create action" do
-          before { post microposts_path }
-          specify { response.should redirect_to(signin_path) }
+          describe "submitting to the create action" do
+            before { post microposts_path }
+            specify { response.should redirect_to(signin_path) }
+          end
+        
+          describe "submitting to the destroy action" do
+            before { delete micropost_path(FactoryGirl.create(:micropost)) }
+           specify { response.should redirect_to(signin_path) }
+          end
+          
         end
         
-        describe "submitting to the destroy action" do
-          before { delete micropost_path(FactoryGirl.create(:micropost)) }
-          specify { response.should redirect_to(signin_path) }
+        describe "in the Relationships controller" do
+          describe "submitting to the create action" do
+            before { post relationships_path }
+            specify { response.should redirect_to(signin_path) }
+          end
+          
+          describe "submitting to the destroy action" do
+            before { delete relationship_path(1) }
+            specify { response.should redirect_to(signin_path) }
+          end
         end
+        
       end
 
-      describe "as wrong user" do
-        let(:user) { FactoryGirl.create(:user) }
-        let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
-        before { sign_in user }
+        describe "as wrong user" do
+          let(:user) { FactoryGirl.create(:user) }
+          let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+          before { sign_in user }
         
-        describe "visiting Users#edit page" do
-          before { visit edit_user_path(wrong_user) }
-          it { should_not have_selector('title', text: full_title('Edit user')) }
-        end
+          describe "visiting Users#edit page" do
+            before { visit edit_user_path(wrong_user) }
+            it { should_not have_selector('title', text: full_title('Edit user')) }
+          end
         
-        describe "submitting a PUT request to the Users#update action" do
-          before { put user_path(wrong_user) }
-          specify { response.should redirect_to(root_path) }
-        end
+          describe "submitting a PUT request to the Users#update action" do
+            before { put user_path(wrong_user) }
+            specify { response.should redirect_to(root_path) }
+          end
       end
       
       describe "as non-admin user" do
